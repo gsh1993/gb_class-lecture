006d890f69322ce535fa2aa8f3db463196dd487c
gs1993 <gauravsharmags1000@gmail.com>
2018-03-18 09:16:22 +0530

    third commit

--- myscript_gs.py
+++ myscript_gs.py
@@ -1,6 +1,13 @@
-x=9.0
+x=10000.0
 s=1.0
-for k in range(6):
+kmax=100
+tol=1.0e-14
+for k in range(kmax):
+    s0=s
     print "before iteration %s, s = %s" %(k,s)
     s=0.5*(s+x/s)
+    delta_s=s-s0
+    if abs(delta_s/x)<tol:
+	break
+
 print "after %s iterations, s=%s" %(k+1,s)
