1f10c131644171281eeefdc3bcc11a740f6f8c7b
gs1993 <gauravsharmags1000@gmail.com>
2018-03-18 09:36:04 +0530

    sqrt2 function made

--- myscript_gs.py
+++ myscript_gs.py
@@ -1,13 +1,12 @@
-x=10000.0
-s=1.0
-kmax=100
-tol=1.0e-14
-for k in range(kmax):
-    s0=s
-    print "before iteration %s, s = %s" %(k,s)
-    s=0.5*(s+x/s)
-    delta_s=s-s0
-    if abs(delta_s/x)<tol:
-	break
-
-print "after %s iterations, s=%s" %(k+1,s)
+def sqrt2(x):
+	s=1.0
+	kmax=100
+	tol=1.0e-14
+	for k in range(kmax):
+	    s0=s
+	    print "before iteration %s, s = %20.15f" %(k,s)
+	    s=0.5*(s+x/s)
+	    delta_s=s-s0
+	    if abs(delta_s/x)<tol:
+		break
+	print "after %s iterations, s=%20.15f" %(k+1,s)
